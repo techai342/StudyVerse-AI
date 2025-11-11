6731ff883499bbd47705be00806c26f283c507c8
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:34:53 +0500

    Create MotivationBooster.jsx

---
+++ src/components/MotivationBooster.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MotivationBooster() {
+  return (
+    <div className="bg-white p-4 rounded shadow mt-4">
+      <h3 className="font-bold">Motivation Booster</h3>
+      <p>"Keep learning, keep growing!"</p>
+    </div>
+  );
+}
+
+export default MotivationBooster;

329414af67c2d0973e776f5457eca0347eee5fba
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:33:21 +0500

    Create CurrentReminder.jsx

---
+++ src/components/CurrentReminder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CurrentReminder() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h3 className="font-bold mb-2">Current Reminder</h3>
+      <p>No reminders for now.</p>
+    </div>
+  );
+}
+
+export default CurrentReminder;

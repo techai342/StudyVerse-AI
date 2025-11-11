dffbb30f12749bc69b04402c5a36acf1b0285752
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:34:11 +0500

    Create DailyGoalTracker.jsx

---
+++ src/components/DailyGoalTracker.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DailyGoalTracker() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h3 className="font-bold mb-2">Daily Goals</h3>
+      <p>Tasks completed: 0/5</p>
+    </div>
+  );
+}
+
+export default DailyGoalTracker;

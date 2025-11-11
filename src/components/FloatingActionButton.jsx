572db1e64443f635c8b3210f13809e1dd593c655
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:35:27 +0500

    Create FloatingActionButton.jsx

---
+++ src/components/FloatingActionButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FloatingActionButton() {
+  return (
+    <button className="fixed bottom-16 right-5 bg-blue-600 text-white p-4 rounded-full shadow-lg">
+      +
+    </button>
+  );
+}
+
+export default FloatingActionButton;

deae1fd492b4a6098783b243bb4d3dc1e0a10145
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:30:19 +0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center p-4 mt-10">
+      © 2025 StudyVerse AI BY Mr SAQIB
+    </footer>
+  );
+}
+
+export default Footer;

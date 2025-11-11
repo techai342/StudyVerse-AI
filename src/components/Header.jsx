5e7c6f6da8330993a064c52e9b70afe459868b46
techai342 <tiktokusr4400@gmail.com>
2025-11-11 21:28:58 +0500

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 text-center text-2xl font-bold">
+      StudyVerse AI
+    </header>
+  );
+}
+
+export default Header;
